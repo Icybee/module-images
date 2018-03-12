543ee6fa00b9a2f42e9f360f8ddf900abd3b2fbf
Olivier Laviale <olivier.laviale@gmail.com>
2018-03-12 23:28:39 +0100

    Use new route definitions

--- config/routes.php
+++ config/routes.php
@@ -7,68 +7,73 @@ use ICanBoogie\Operation;
 
 use Icybee\Modules\Images\Operation\WidgetOperation;
 use Icybee\Routing\RouteMaker as Make;
+use ICanBoogie\Routing\RouteDefinition as Route;
 
 return [
 
 	'images:show' => [
 
-		'pattern' => '/images/<uuid:{:uuid:}><extension:[\.a-z]*>',
-		'controller' => ImagesController::class . '#show',
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/<uuid:{:uuid:}><extension:[\.a-z]*>',
+		Route::CONTROLLER => ImagesController::class,
+		Route::ACTION => Make::ACTION_SHOW,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'images:download' => [
 
-		'pattern' => '/images/download/<uuid:{:uuid:}><extension:[\.a-z]*>',
-		'controller' => ImagesController::class . '#download',
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/download/<uuid:{:uuid:}><extension:[\.a-z]*>',
+		Route::CONTROLLER => ImagesController::class,
+		Route::ACTION => 'download',
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'images:protected:show' => [
 
-		'pattern' => '/images/<nid:\d+><extension:[\.a-z]*>',
-		'controller' => ImagesAdminController::class . '#show'
+		Route::PATTERN => '/images/<nid:\d+><extension:[\.a-z]*>',
+		Route::CONTROLLER => ImagesAdminController::class,
+		Route::ACTION => 'show',
 
 	],
 
 	'images:protected:download' => [
 
-		'pattern' => '/images/download/<nid:\d+><extension:[\.a-z]*>',
-		'controller' => ImagesAdminController::class . '#download'
+		Route::PATTERN => '/images/download/<nid:\d+><extension:[\.a-z]*>',
+		Route::CONTROLLER => ImagesAdminController::class,
+		Route::ACTION => 'download',
 
 	],
 
 	'images/thumbnail' => [
 
-		'pattern' => '/images/<uuid:{:uuid:}>/<size:\d+x\d+|\d+x|x\d+>*',
-		'controller' => ThumbnailController::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/<uuid:{:uuid:}>/<size:\d+x\d+|\d+x|x\d+>*',
+		Route::CONTROLLER => ThumbnailController::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'images/thumbnail-version' => [
 
-		'pattern' => '/images/<uuid:{:uuid:}>/thumbnails/:version',
-		'controller' => ThumbnailController::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/<uuid:{:uuid:}>/thumbnails/:version',
+		Route::CONTROLLER => ThumbnailController::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'protected:images/thumbnail' => [
 
-		'pattern' => '/images/<nid:\d+>/<size:\d+x\d+|\d+x|x\d+>*',
-		'controller' => ThumbnailController::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/<nid:\d+>/<size:\d+x\d+|\d+x|x\d+>*',
+		Route::CONTROLLER => ThumbnailController::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'protected:images/thumbnail-version' => [
 
-		'pattern' => '/images/<nid:\d+>/thumbnails/:version',
-		'controller' => ThumbnailController::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/images/<nid:\d+>/thumbnails/:version',
+		Route::CONTROLLER => ThumbnailController::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
@@ -78,17 +83,17 @@ return [
 
 	'api:widgets:adjust-image:popup' => [
 
-		'pattern' => '/api/widgets/adjust-image/:mode',
-		'controller' => WidgetOperation::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/api/widgets/adjust-image/:mode',
+		Route::CONTROLLER => WidgetOperation::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
 	'api:widgets:adjust-thumbnail:popup' => [
 
-		'pattern' => '/api/widgets/adjust-thumbnail/:mode',
-		'controller' => WidgetOperation::class,
-		'via' => Request::METHOD_GET
+		Route::PATTERN => '/api/widgets/adjust-thumbnail/:mode',
+		Route::CONTROLLER => WidgetOperation::class,
+		Route::VIA => Request::METHOD_GET,
 
 	],
 
@@ -98,15 +103,15 @@ return [
 
 	'redirect:admin/resources' => [
 
-		'pattern' => '/admin/resources',
-		'location' => '/admin/images'
+		Route::PATTERN => '/admin/resources',
+		Route::LOCATION => '/admin/images',
 
 	]
 
 ] + Make::admin('images', ImagesAdminController::class, [
 
-	'id_name' => 'nid',
-	'actions' => [
+	Make::OPTION_ID_NAME => 'nid',
+	Make::OPTION_ACTIONS => [
 
 		'gallery' => [ '/{name}/gallery', Request::METHOD_ANY ]
 
